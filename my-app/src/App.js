d0ee5ba462d51f6fd3f4f30fcd665e98e5cf780f
yuuhi <hainm112123@gmail.com>
2023-06-09 10:37:18 +0700

    item-click feature

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,20 +7,48 @@ import { Component } from 'react';
 class App extends Component {
   constructor() {
     super();
-    this.todoItem = [
-      {title: "Anime", isComplete: true},
-      {title: "Manga", isComplete: true},
-      {title: "Game", isComplete: false},
-    ];
+    this.state = {
+      todoItems : [
+        {title: "Anime", isComplete: true},
+        {title: "Manga", isComplete: true},
+        {title: "Game", isComplete: false},
+      ],
+    }
+
+    // this.onItemClicked = this.onItemClicked.bind(this);
+  }
+
+  onItemClicked(item) {
+    return () => {
+      const isComplete = item.isComplete;
+      const todoItems = this.state.todoItems;
+      const index = todoItems.indexOf(item);
+      this.setState({
+        todoItems: [
+          ...todoItems.slice(0, index),
+          {
+            ...item,
+            isComplete: !isComplete,
+          },
+          ...todoItems.slice(index + 1)
+        ]
+      })
+    }
   }
 
   render() {
+    const todoItems = this.state.todoItems;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           {
-            this.todoItem.length === 0 ? 'Nothing' : this.todoItem.map((item, index) => <TodoItem key={index} item={item}></TodoItem>)
+            todoItems.length === 0 ? 'Nothing' : todoItems.map((item, index) => 
+            <TodoItem 
+              key={index} 
+              item={item} 
+              onClick={this.onItemClicked(item)}>
+            </TodoItem>)
           }       
           <p>
             Edit <code>src/App.js</code> and save to reload.
--- my-app/src/components/TodoItem.js
+++ my-app/src/components/TodoItem.js
@@ -4,11 +4,12 @@ import './TodoItem.css'
 class TodoItem extends Component {
   render() {
     const item = this.props.item;
+    // console.log(this.props.index);
     let className = "TodoItem";
     if (item.isComplete) className += " TodoItem--Complete"
 
     return (
-      <div className={className}>
+      <div className={className} data-item-index={this.props.index} onClick={this.props.onClick}>
         <p>{item.title}</p>
       </div>
     );
